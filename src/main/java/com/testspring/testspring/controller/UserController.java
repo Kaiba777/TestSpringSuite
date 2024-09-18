980f8c167656e3263e2a0ff720b68d2ca3d21714
Kaiba 777 <sawadogomartial222@gmail.com>
2024-09-18 15:25:43 +0000

    Implémentation de la page de profile dans le compte utilisateur

--- src/main/java/com/testspring/testspring/controller/UserController.java
+++ src/main/java/com/testspring/testspring/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.testspring.testspring.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -91,18 +92,46 @@ public class UserController {
             Object principal = authentication.getPrincipal();
 
             String nom;
+            String prenom;
             String image;
+            String age;
+            String sexe;
+            String numero;
+            String role;
+            String email;
+            Date createdAt;
             if (principal instanceof CustomUserDetails) {
                 CustomUserDetails userDetails = (CustomUserDetails) principal;
                 nom = userDetails.getNom(); // Récupérer le nom
+                prenom = userDetails.getPrenom(); // Récupérer le prénom
                 image = userDetails.getImage(); // Récupérer l'image
+                age = userDetails.getAge(); // Récupérer l'âge
+                sexe = userDetails.getSexe(); // Récupérer le sexe
+                numero = userDetails.getNumero(); // Récupérer le numéro
+                role = userDetails.getRole(); // Récupérer le role
+                email = userDetails.getUsername(); // Récupérer l'email
+                createdAt = userDetails.getCreatedAt(); // Récupérer le nom
             } else {
                 nom = "Inconnu";
+                prenom = "Inconnu";
                 image = "Inconnu";
+                age = "Inconnu";
+                sexe = "Inconnu";
+                numero = "Inconnu";
+                role = "Inconnu";
+                email = "Inconnu";
+                createdAt = new Date(); // Valeur par défaut
             }
 
             model.addAttribute("nom", nom);
+            model.addAttribute("prenom", prenom);
             model.addAttribute("image", image);
+            model.addAttribute("age", age);
+            model.addAttribute("sexe", sexe);
+            model.addAttribute("numero", numero);
+            model.addAttribute("role", role);
+            model.addAttribute("email", email);
+            model.addAttribute("createdAt", createdAt);
         }
         return "private/user/pages-profile";
     }
